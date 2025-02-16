47a8f70155f10d5e0586ad4c72a5aa03bf6b4531
jjhw <jjhw@nexonesoft.com>
2025-02-16 23:55:52 +0900

    도메인 모델 패턴으로 설계
    Item Service, Repository 추가

--- jpashop/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ jpashop/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,7 +12,7 @@ import java.util.List;
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
-@Getter @Setter
+@Getter // @Setter Setter를 가지고 변환하는게 아닌 비즈니스 로직을 통해 변경하는게 객체지향 적인것
 public abstract class Item {
     @Id @GeneratedValue
     @Column(name = "item_id")
@@ -24,4 +25,23 @@ public abstract class Item {
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
 
+    // ===== 비즈니스 로직 =====
+
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
