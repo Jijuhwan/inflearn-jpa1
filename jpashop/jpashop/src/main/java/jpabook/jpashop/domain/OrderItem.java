de554f0e5b87d66320cabc389d3bb6c093e53b12
jjhw <jjhw@nexonesoft.com>
2025-02-16 23:56:06 +0900

    도메인 모델 패턴으로 설계
    Item Service, Repository 추가

--- jpashop/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpashop/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -2,13 +2,16 @@ package jpabook.jpashop.domain;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import static jakarta.persistence.FetchType.LAZY;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)  // 임의로 Set 형식으로 생성하는것을 막을 수 있다.(직접 생성 방지)
 public class OrderItem {
     @Id @GeneratedValue
     @Column(name = "order_item_id")
@@ -25,4 +28,23 @@ public class OrderItem {
     private int orderPrice;
 
     private int count;
+
+    // ===== 비즈니스 로직 =====
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count){
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
