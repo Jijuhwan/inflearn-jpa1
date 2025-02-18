3a5f4847530be5aa34f4cc9e94e34ef17196c843
jjhw <otl9882@naver.com>
2025-02-19 07:29:54 +0900

    더티체킹, 준영속 엔티티 구현
    1. 더티체킹은 트랜잭션 커밋 당시 변경된 데이터를 감지하여, 업데이트 날림
    2. 준영속 엔티티는 더이상 관리하지 않는 엔티티이며, DB에 다녀온 데이터임(식별자 존재)
    3. 준영속 엔티티 수정 방법으론 더티체킹으로 변경하거나 머지 사용(PUT)

--- jpashop/jpashop/src/main/java/jpabook/jpashop/controller/ItemController.java
+++ jpashop/jpashop/src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -58,16 +58,21 @@ public class ItemController {
     }
 
     @PostMapping("{itemId}/edit")
-    public String updateItem(@PathVariable String itemId, @ModelAttribute("form") BookForm form){
-        Book book = new Book();
-        book.setId(form.getId());
-        book.setName(form.getName());
-        book.setPrice(form.getPrice());
-        book.setStockQuantity(form.getStockQuantity());
-        book.setAuthor(form.getAuthor());
-        book.setIsbn(form.getIsbn());
+    public String updateItem(@PathVariable Long itemId, @ModelAttribute("form") BookForm form){
+//        Book book = new Book();
+//        book.setId(form.getId());
+//        book.setName(form.getName());
+//        book.setPrice(form.getPrice());
+//        book.setStockQuantity(form.getStockQuantity());
+//        book.setAuthor(form.getAuthor());
+//        book.setIsbn(form.getIsbn());
 
-        itemService.saveItem(book);
+        // merge 사용
+        // itemService.saveItem(book);
+
+        // 더티 체킹 사용
+        itemService.updateItem(itemId, form.getName(), form.getPrice(), form.getStockQuantity());
+
         return "redirect:/items";
     }
 }
--- jpashop/jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
+++ jpashop/jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +18,15 @@ public class ItemService {
     @Transactional  // readonly이면 save가 안먹어서 별도 선언
     public void saveItem(Item item){
         itemRepository.save(item);
+    }
+
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity ){//UpdateDto updateDto) {
+        Item findItem = itemRepository.findOne(itemId);
+        findItem.setPrice(price);
+        findItem.setName(name);
+        findItem.setStockQuantity(stockQuantity);
+        // findItem이 영속된 상태이기에 Transactional이 해당 메소드가 끝나면 commit을 날린다.
     }
 
     public List<Item> findItems() {
