29ff041198b6a2fd9cca2dc746d7a26df302abc9
jjhw <jjhw@nexonesoft.com>
2025-02-23 12:07:26 +0900

    V2, V3 기능 구현
    V2 -> entity 외부 노출을 안하도록 DTO 구현
    V3 -> N + 1 문제를 해결하도록 Fetch 조인 활용

--- jpashop/jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ jpashop/jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,13 +1,18 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 아래 정도만 연관이 걸리도록 구현
@@ -30,5 +35,46 @@ public class OrderSimpleApiController {
             order.getDelivery().getAddress();    // Lazy 강제 초기화
         }
         return all;
+    }
+
+    // V1 문제 해결 -> entity 외부 노출 해결
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2(){
+        // N + 1 -> 1(order 조회) + 회원 N(지연 로딩 조회 N번) + 배송 N 문제 발생(지연 로딩 조회 N번)
+        // 1개의 쿼리 결과로 N개가 나오는데 그만큼 추가 쿼리 발생한다는 뜻.
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+        return result;
+    }
+
+    // V2 문제 해결 -> N + 1 문제 해결 -> fetch 조인으로 쿼리 1번으로 해결함.
+    @GetMapping("/api/v3/simple-orders")
+    public List<SimpleOrderDto> ordersV3(){
+        // FETCH 조인으로 대부분 JPA 성능 문제 해결됨.
+        List<Order> orders = orderRepository.findAllWithMemberDelivery();
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
+    @Data
+    static class SimpleOrderDto{
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();         // LAZY 초기화 (영속성 컨텍스트에서 찾는데 없으면 쿼리를 날림)
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+        }
     }
 }
