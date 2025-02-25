46ac1fd5db3b017427a2846f48ecd55dbd848098
jjhw <otl9882@naver.com>
2025-02-26 08:16:22 +0900

    컬렉션 데이터 페이징 조회 기능 추가
    hibernate.default_batch_size를 yml 파일에 추가하여, WHERE 문에 IN 절을 사용하여, 데이터를 가져오면 된다.

--- jpashop/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ jpashop/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -9,6 +9,7 @@ import jpabook.jpashop.repository.OrderSearch;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -63,6 +64,22 @@ public class OrderApiController {
                 .collect(Collectors.toList());
         return result;
     }
+
+    @GetMapping("/api/v3.1/orders")
+    public List<OrderDto> orderV3_page(
+            @RequestParam(value = "offset", defaultValue = "0") int offset,
+            @RequestParam(value = "limit", defaultValue = "100") int limit ) {
+
+        List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
+
+        List<OrderDto> result = orders.stream()
+                .map(OrderDto::new)
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
+
 
     @Getter
     static class OrderDto{
